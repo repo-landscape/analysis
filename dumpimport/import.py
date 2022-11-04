819f303a9bf81a25bd30fd075c62dc380e210306
Mateusz Żółtak <zozlak@zozlak.org>
2022-11-04 12:09:32 +0100

    dumpimport/import.py: implement relations import (have different data structure)

--- dumpimport/import.py
+++ dumpimport/import.py
@@ -10,6 +10,7 @@ parser.add_argument('--dbConf', default='user=postgres host=127.0.0.1')
 parser.add_argument('--dbPswd')
 parser.add_argument('--filesLimit', type=int)
 parser.add_argument('--filesOffset', type=int)
+parser.add_argument('--truncate', action='store_true', help='Should the database table be truncated before the import')
 parser.add_argument('--verbose', action='store_true')
 parser.add_argument('entityType', choices=['communities_infrastructures', 'datasets', 'datasources', 'organizations', 'other_research_products', 'projects', 'publications', 'relations', 'software'])
 parser.add_argument('jsonFileOrDir')
@@ -19,6 +20,9 @@ logging.basicConfig(stream=sys.stdout, format='%(levelname)s:%(asctime)s: %(mess
 
 con = psycopg2.connect(args.dbConf + (f' password= {args.dbPswd}' if args.dbPswd else ''))
 cur = con.cursor()
+if args.truncate:
+    cur.execute("TRUNCATE {args.entityType}")
+    con.commit()
 if os.path.isfile(args.jsonFileOrDir):
     files = [args.jsonFileOrDir]
 else:
@@ -35,9 +39,20 @@ for file in files:
     with open(file, 'r') as fh:
         for line in fh:
             data = json.loads(line)
-            cur.execute(
-                f"INSERT INTO {args.entityType} (id, data) VALUES (%s, %s)",
-                (data['id'], line)
-            )
+            if args.entityType == 'relations':
+                cur.execute(
+                    f"""
+                    INSERT INTO {args.entityType} (sourceid, targetid, reltype, sourcetype, targettype, data) 
+                    VALUES (%s, %s, %s, %s, %s, %s) 
+                    ON CONFLICT (sourceid, targetid) DO UPDATE SET 
+                        reltype = EXCLUDED.reltype, sourcetype = EXCLUDED.sourcetype, targettype = EXCLUDED.targettype, data = EXCLUDED.data
+                    """,
+                    (data['source']['id'], data['target']['id'], data['reltype']['name'], data['source']['type'], data['target']['type'], line)
+                )
+            else:
+                cur.execute(
+                    f"INSERT INTO {args.entityType} (id, data) VALUES (%s, %s) ON CONFLICT (id) DO UPDATE SET data = EXCLUDED.data",
+                    (data['id'], line)
+                )
     con.commit()
 
