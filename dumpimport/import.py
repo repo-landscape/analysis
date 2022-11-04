ac33db165266a9e21b593ab12cdd7fc5aed65e9a
Mateusz Żółtak <zozlak@zozlak.org>
2022-11-04 12:30:52 +0100

    dumpimport/import.py: add error handling, handle \u0000 in strings and display ingestion time summary

--- dumpimport/import.py
+++ dumpimport/import.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import json
 import logging
 import os
@@ -17,6 +18,7 @@ parser.add_argument('jsonFileOrDir')
 args = parser.parse_args()
 
 logging.basicConfig(stream=sys.stdout, format='%(levelname)s:%(asctime)s: %(message)s', level=logging.DEBUG if args.verbose else logging.INFO)
+T0 = datetime.datetime.now()
 
 con = psycopg2.connect(args.dbConf + (f' password= {args.dbPswd}' if args.dbPswd else ''))
 cur = con.cursor()
@@ -36,23 +38,29 @@ nFile = 0
 for file in files:
     nFile += 1
     logging.info(f'Processing file {file} ({nFile}/{len(files)})')
-    with open(file, 'r') as fh:
-        for line in fh:
-            data = json.loads(line)
-            if args.entityType == 'relations':
-                cur.execute(
-                    f"""
-                    INSERT INTO {args.entityType} (sourceid, targetid, reltype, sourcetype, targettype, data) 
-                    VALUES (%s, %s, %s, %s, %s, %s) 
-                    ON CONFLICT (sourceid, targetid) DO UPDATE SET 
-                        reltype = EXCLUDED.reltype, sourcetype = EXCLUDED.sourcetype, targettype = EXCLUDED.targettype, data = EXCLUDED.data
-                    """,
-                    (data['source']['id'], data['target']['id'], data['reltype']['name'], data['source']['type'], data['target']['type'], line)
-                )
-            else:
-                cur.execute(
-                    f"INSERT INTO {args.entityType} (id, data) VALUES (%s, %s) ON CONFLICT (id) DO UPDATE SET data = EXCLUDED.data",
-                    (data['id'], line)
-                )
-    con.commit()
-
+    try:
+        with open(file, 'r') as fh:
+            for line in fh:
+                line = line.replace('\\u0000', '')
+                data = json.loads(line)
+                if args.entityType == 'relations':
+                    cur.execute(
+                        f"""
+                        INSERT INTO {args.entityType} (sourceid, targetid, reltype, sourcetype, targettype, data) 
+                        VALUES (%s, %s, %s, %s, %s, %s) 
+                        ON CONFLICT (sourceid, targetid) DO UPDATE SET 
+                            reltype = EXCLUDED.reltype, sourcetype = EXCLUDED.sourcetype, targettype = EXCLUDED.targettype, data = EXCLUDED.data
+                        """,
+                        (data['source']['id'], data['target']['id'], data['reltype']['name'], data['source']['type'], data['target']['type'], line)
+                    )
+                else:
+                    cur.execute(
+                        f"INSERT INTO {args.entityType} (id, data) VALUES (%s, %s) ON CONFLICT (id) DO UPDATE SET data = EXCLUDED.data",
+                        (data['id'], line)
+                    )
+        con.commit()
+    except Exception as e:
+        logging.error(e)
+        con.rollback()
+T0 = (datetime.datetime.now() - T0).total_seconds()
+logging.info(f'{nFile} processed in {T0} s (on average {T0 / nFile} per file)')
