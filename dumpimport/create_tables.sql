eb57b50b5739769bd6def41c37213d2ba86b42e6
Mateusz Żółtak <zozlak@zozlak.org>
2022-11-22 09:22:53 +0100

    dumpimport/create_tables.sql: create indices separately

--- dumpimport/create_tables.sql
+++ dumpimport/create_tables.sql
@@ -1,4 +1,4 @@
-create table entities (id text, type text, data jsonb, primary key (id, type)) partition by list (type);
+create table entities (id text, type text, data jsonb) partition by list (type);
 create table communities_infrastructures partition of entities for values in ('communities_infrastructures');
 create table datasets partition of entities for values in ('dataset');
 create table datasources partition of entities for values in ('datasource');
@@ -7,4 +7,18 @@ create table other_research_products partition of entities for values in ('other
 create table projects partition of entities for values in ('project');
 create table publications partition of entities for values in ('publication');
 create table software partition of entities for values in ('software');
-create table relations (sourceid text, targetid text, reltype text, data jsonb not null, primary key (sourceid, targetid));
+create table relations (sourceid text, targetid text, reltype text, data jsonb not null);
+
+-- preferably after data ingestion
+create index communities_infrastructures_id_idx on communities_infrastructures using btree(id);
+create index datasets_id_idx on datasets using brin(id);
+create index datasources_id_idx on datasources using btree(id);
+create index organizations_id_idx on organizations using btree(id);
+create index other_research_products_id_idx on other_research_products using brin(id);
+create index projects_id_idx on projects using btree(id);
+create index publications_id_idx on publications using brin(id);
+create index software_id_idx on software using btree(id);
+create index relations_sourceid_idx on relations using brin(sourceid);
+create index relations_targetid_idx on relations using brin(targetid);
+create index relations_reltype_idx on relations using brin(reltype);
+
